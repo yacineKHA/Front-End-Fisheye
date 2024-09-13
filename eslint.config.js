ffb18fae2dc5b3e46da0f9118b106e6f11518d46
yacineKHA <yacine.kad@outlook.fr>
2024-09-13 17:59:49 +0200

    Update EsLint

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+      files: ['**/*.js'],
+      languageOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+      rules: {
+        'no-unused-vars': 'warn',
+        'eqeqeq': 'error',
+      },
+    },
+  ];
+  
